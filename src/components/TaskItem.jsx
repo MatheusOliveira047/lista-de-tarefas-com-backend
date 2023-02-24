20bc85369b90e074f423e5f5f35b90ee839d937b
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-02-24 20:42:22 -0300

    feat: add completed task

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -14,20 +14,32 @@ const TaskItem = ({task,getTasks}) => {
 
   const handleDeleteTask = async (id)=>{
     setLoading(true)
-    
     try {
         await axios.delete(`https://lista-de-tarefas-4vhh.onrender.com/api/tasks/delete/${id}`)
 
+        await getTasks()
         alert.success('Tarefa removida com sucesso !!!')
         setLoading(false)
-        getTasks()
       } catch (error) {
         setLoading(false)
         alert.error('ops ocorreu um erro, tente novamente mais tarde !!!')
       }
-
   }
 
+  const handleCompleted =async (e)=>{
+    try {
+
+      await axios.patch(`https://lista-de-tarefas-4vhh.onrender.com/api/tasks/update/${task._id}`,{
+        isCompleted: e.target.checked,
+      })
+
+      await getTasks()
+      alert.success('Tarefa concluida!!!')
+
+    } catch (error) {
+      alert.error('ops ocorreu um erro, tente novamente mais tarde !!!')
+    }
+  }
 
   return (
     <div className="task-item-container">
@@ -42,7 +54,8 @@ const TaskItem = ({task,getTasks}) => {
           </span>
           <input 
             type="checkbox" 
-            defaultChecked={task.isCompleted} 
+            defaultChecked={task.isCompleted}
+            onChange={(e)=>handleCompleted(e)} 
           />
           <span className={task.isCompleted ? 'checkmark completed' : 'checkmark'}></span>
         </label>
