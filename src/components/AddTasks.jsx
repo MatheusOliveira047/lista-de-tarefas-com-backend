077b11fc4dd23840bbed881923eda6c37e285a2b
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-02-23 22:24:06 -0300

    feat: add react-alert templete

--- src/components/AddTasks.jsx
+++ src/components/AddTasks.jsx
@@ -1,12 +1,30 @@
-import './AddTasks.scss'
-import CustomInput from './CustomInput'
+import {FaPlus} from 'react-icons/fa'
 import { useState } from 'react'
+import axios from 'axios'
+
+import './AddTasks.scss'
+
+import CustomInput from './CustomInput'
+import CustomButton from './CustomButton'
+import {useAlert} from 'react-alert' 
 
 const AddTask = ()=>{
   const [task,setTask] = useState('')
 
+  const alert = useAlert()
+
   const onChange = (e)=>{
     setTask(e.target.value)
+  }
+
+  const handleTaskAddition = async()=>{
+    try {
+      if(task.length === 0){
+        return alert.error('A tarefa precisa de uma descrição para ser adicionada.')
+      }
+    } catch (error) {
+      
+    }
   }
 
   return(
@@ -16,6 +34,9 @@ const AddTask = ()=>{
         value={task} 
         onChange={onChange}
       />
+      <CustomButton onClick={handleTaskAddition}>
+        <FaPlus size={14} color="#fff"/>
+      </CustomButton>
     </div>
   )
 }
