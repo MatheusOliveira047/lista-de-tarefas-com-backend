ee9f0f0557ace9fa1181f92d6df7dd4e97a3d5cd
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-02-26 22:14:41 -0300

    feat: add navigation to login and home

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,24 @@
 import './Sidebar.scss'
 import logo from '../assets/images/logo.png'
+import {useNavigate} from 'react-router-dom'
+
 
 import CustomButtom from './CustomButton'
 
 const Sidebar = ()=>{
+  const navigate = useNavigate()
+
+  const handleSignout = ()=>{
+    navigate('/login')
+  }
+
   return(
     <div className="sidebar-container">
     <div className="logo">
       <img src={logo} alt="Full Stask club"/>
     </div>
     <div className="sign-out">
-      <CustomButtom>
+      <CustomButtom onClick={handleSignout}>
         SAIR
       </CustomButtom>
     </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,21 @@
 import './Login.scss'
 import Logo from '../assets/images/logo-2.png'
 
+import {useNavigate} from 'react-router-dom'
 import CustomButton from '../components/CustomButton'
 
 const Login = ()=>{
+  const navigate = useNavigate()
+
+  const handleSignin = ()=>{
+    navigate('/')
+  }
+
   return(
     <div className="login-container">
       <img src={Logo} alt="logo" />
       <div className="button-container">
-        <CustomButton>
+        <CustomButton onClick={handleSignin}>
           Entrar
         </CustomButton>
       </div>
